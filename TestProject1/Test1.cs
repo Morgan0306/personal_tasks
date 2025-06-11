cee78c1d7b7b454bc70767d044d8d81ddc26fbea
Morgan0306 <0354689@gmail.com>
2025-06-11 13:50:46 +0800

    CreateTestProject

---
+++ TestProject1/Test1.cs
@@ -0,0 +1,18 @@
+﻿namespace TestProject1
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            int expected = 1;
+            int actual = 1;
+
+            // Act & Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
